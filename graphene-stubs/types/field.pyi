c4e712b8b968003bb3ff82e478adbacf0713fbcb
Cohen Karnell <cohen@trialspark.com>
2020-05-21 16:56:54 -0400

    Add types to field

--- graphene-stubs/types/field.pyi
+++ graphene-stubs/types/field.pyi
@@ -1,36 +1,40 @@
+from enum import Enum as PyEnum
+from typing import Any, Optional, Union, Dict, Callable, Type
 from .argument import Argument as Argument, to_arguments as to_arguments
 from .mountedtype import MountedType as MountedType
 from .structures import NonNull as NonNull
 from .unmountedtype import UnmountedType as UnmountedType
 from .utils import get_type as get_type
-from typing import Any, Optional
+from .objecttype import ObjectType
+from .structures import Structure
+from .base import BaseType
+from .enum import Enum
 
 base_type = type
-
 
 def source_resolver(source: Any, root: Any, info: Any, **args: Any) -> Any:
     ...
 
-
+TypeType: Union[Type[Union[ObjectType, UnmountedType, BaseType, Enum, PyEnum]], Structure, Enum, str]
 class Field(MountedType):
-    name: Any = ...
-    args: Any = ...
-    resolver: Any = ...
-    deprecation_reason: Any = ...
-    description: Any = ...
-    default_value: Any = ...
+    name: Optional[str] = ...
+    args: Optional[Dict[str, Argument]] = ...
+    resolver: Optional[Callable[[], Any]] = ...
+    deprecation_reason: Optional[str] = ...
+    description: Optional[str] = ...
+    default_value: Optional[Any] = ...
 
     def __init__(
         self,
-        type: Any,
-        args: Optional[Any] = ...,
-        resolver: Optional[Any] = ...,
-        source: Optional[Any] = ...,
-        deprecation_reason: Optional[Any] = ...,
-        name: Optional[Any] = ...,
-        description: Optional[Any] = ...,
-        required: bool = ...,
-        _creation_counter: Optional[Any] = ...,
+        type: Union[TypeType, Callable[[], TypeType]],
+        args: Optional[Dict[str, Argument]] = None,
+        resolver: Optional[Callable[[], Any]] = None,
+        source: Optional[str] = None,
+        deprecation_reason: Optional[str] = None,
+        name: Optional[Union[str, Argument]] = None,
+        description: Optional[str] = None,
+        required: bool = False,
+        _creation_counter: Optional[Any] = None,
         default_value: Optional[Any] = ...,
         **extra_args: Any
     ) -> None:
